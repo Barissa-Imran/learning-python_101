7e7d6ea15542804c42457e2525837fbf6bd69b5a
MissJuly <joygitau98@gmail.com>
2023-01-03 19:56:06 +0300

    Add strings file

---
+++ strings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str0 = "Twinkle, twinkle, little star,"
+str1 = "How i wonder what you are!"
+str2 = "Up above the world so high,"
+str3 = "Like a diamond in the sky."
+#str = ()
+print(f"{str0}\n\t{str1}\n\t\t{str2}\n\t\t{str3}\n{str0}\n\t{str1}")
