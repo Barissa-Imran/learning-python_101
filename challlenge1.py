2f0231bd21a68e9639c9760d0d81420fffb61ee1
Barissa-Imran <imranbarissa@gmail.com>
2023-04-02 14:32:48 +0300

    Add string exercise

---
+++ challlenge1.py
@@ -0,0 +1,8 @@
+# nums = []
+for i in range(1500, 2700):
+    if i % 7 == 0 and i % 5 == 0:
+        # nums.append(i)
+        print(i, end=", ")
+
+# print(nums)
+print()
---
+++ strings1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Calculates the length of a string"""
+
+
+string = input("Enter a string: ")
+name = "Joy"
+print("The length of the string is: {} + {}".format(len(string), name))
+
+
+print(f"The length of the string is: {len(string)} + {name} ")
